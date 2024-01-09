e3184e2b6a0cfc8eb1ec27491bfd7bc9e6639a09
Yuri <yurifl03@gmail.com>
2024-01-09 10:56:45 -0300

    wip

--- nix/flake/flake.nix
+++ nix/flake/flake.nix
@@ -6,17 +6,48 @@
     # nix-infect usage guide at the time of writing. And nix-infect was
     # what I used to install NixOS on my remote machine. 
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
   };
 
-  outputs = { self, ... }@inputs: {
-    nixopsConfigurations.default = {
-      inherit (inputs) nixpkgs; # required! nixops complains if not present
-      network.storage.legacy = { }; # required! nixops complains if not present
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
 
-      ## TODO: here we will specify all the "regular" NixOps properties,
-      ## like network.description, machine definitions, etc.
-      ## ...
+
+
+
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+    ];
+
+    perSystem = { pkgs, system, ... }: {
+      nixopsConfigurations.default = {
+        inherit (inputs) nixpkgs; # required! nixops complains if not present
+        network.storage.legacy = { }; # required! nixops complains if not present
+
+        ## TODO: here we will specify all the "regular" NixOps properties,
+        ## like network.description, machine definitions, etc.
+        ## ...
+
+      };
+      # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+      packages.default = pkgs.hello;
+
+      # packages.default = pkgs.callPackage ./package.nix { };
+
+      # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+      # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+      # packages.x86_64-darwin.default = self.packages.x86_64-darwin.hello;
+
+      # packages.x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
+
 
     };
+
   };
 }
+
+
