e0b51e0d0ee7f4d57e1d47483ceefb78f888ae0c
Yuri <yurifl03@gmail.com>
2024-01-13 18:56:10 -0300

    wip

--- nix/flake/flake.nix
+++ nix/flake/flake.nix
@@ -50,45 +50,6 @@
             imports = [ ./sd-image.nix ];
           };
 
-          # master =
-          #   { config, pkgs, ... }:
-          #   {
-          #     # https://github.com/NixOS/nixops/blob/master/nix/options.nix
-          #     deployment = {
-          #       targetUser = "nixos";
-          #       provisionSSHKey = true;
-          #       targetHost = "192.168.68.102";
-          #     };
-          #     environment.systemPackages = with pkgs; [ figlet ];
-          #     networking.firewall.allowedTCPPorts = [ 80 22 ];
-          #     services = {
-          #       nginx = {
-          #         enable = true;
-          #         virtualHosts.vhost1 = {
-          #           default = true;
-          #           locations."/" = {
-          #             root = pkgs.writeTextDir "index.html" "Hello akavel's world!";
-          #           };
-          #         };
-          #       };
-          #     };
-          #   };
-
-          # master = { pkgs, ... }:
-          #   (import ./sd-image.nix { }) // {
-          #     deployment.targetHost = "192.168.68.102"; # replace with your machine's IP
-
-          #     networking.hostName = "my-hostname";
-          #     networking.domain = "";
-          #     # Allow SSH through the firewall - TODO: is it required or automatic?
-          #     networking.firewall.allowedTCPPorts = [ 22 ];
-
-          #     services.openssh.enable = true;
-          #     users.users.root.openssh.authorizedKeys.keys = [
-          #       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCfAwxYdoLR6YzoIx2+L593yLGpHaseGTCm3fxrshgD yurifl03@gmail.com"
-          #     ];
-          #   };
-
           master = { pkgs, ... }: {
             deployment = {
               targetUser = "nixos";
@@ -96,13 +57,6 @@
               targetHost = "192.168.68.104";
             };
           };
-
-          # n1 =
-          #   { config, pkgs, ... }:
-          #   {
-          #     # Fake
-          #     deployment.targetHost = "000.000.00.00";
-          #   };
         };
       };
     };
--- nix/nixops/nixops.nix
+++ nix/nixops/nixops.nix
@@ -4,7 +4,8 @@
     # NB this is not really what makes it a legacy network; lack of flakes is.
     storage.legacy = { };
   };
-  server = { lib, pkgs, ... }: {
+  # Machine
+  master = { lib, pkgs, ... }: {
     imports = [ ../sd-image.nix ];
 
     deployment = {
@@ -15,9 +16,37 @@
     };
 
     environment.systemPackages = [ pkgs.hello pkgs.figlet ];
+
+
+    boot.loader.grub.enable = false;
+    boot.loader.generic-extlinux-compatible.enable = true;
+
+
+    boot.initrd.availableKernelModules = [ "usbhid" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    boot.kernelPackages = pkgs.linuxPackages_rpi4;
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+
+    swapDevices = [ ];
   };
 
   defaults = {
+
+    # Enable openssh and add the ssh key to the root user
+    services.openssh.enable = true;
+
+    # Define that we need to build for ARM
+    nixpkgs.localSystem = {
+      system = "aarch64-linux";
+      config = "aarch64-unknown-linux-gnu";
+    };
+
     users.extraUsers.nixos.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCfAwxYdoLR6YzoIx2+L593yLGpHaseGTCm3fxrshgD yurifl03@gmail.com"
     ];
